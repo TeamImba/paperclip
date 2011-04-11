2eb43f4d6948e5327eda551ea19946a791a44697
russell <mrrussellong@yahoo.com>
2011-04-11 15:29:51 +0800

    added jpegtran

--- lib/paperclip/thumbnail.rb
+++ lib/paperclip/thumbnail.rb
@@ -47,7 +47,12 @@ module Paperclip
       src = @file
       dst = Tempfile.new([@basename, @format ? ".#{@format}" : ''])
       dst.binmode
-
+      
+      begin
+        @success = Paperclip.run("jpegtran -copy none -progressive -optimize #{File.expand_path(src.path)}[0] > #{File.expand_path(src.path)}[1]")
+      rescue PaperclipCommandLineError => e
+      end
+      
       begin
         parameters = []
         parameters << source_file_options
@@ -58,7 +63,11 @@ module Paperclip
 
         parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
 
-        success = Paperclip.run("convert", parameters, :source => "#{File.expand_path(src.path)}[0]", :dest => File.expand_path(dst.path))
+        if @success
+          success = Paperclip.run("convert", parameters, :source => "#{File.expand_path(src.path)}[1]", :dest => File.expand_path(dst.path))
+        else
+          success = Paperclip.run("convert", parameters, :source => "#{File.expand_path(src.path)}[0]", :dest => File.expand_path(dst.path))
+        end
       rescue PaperclipCommandLineError => e
         raise PaperclipError, "There was an error processing the thumbnail for #{@basename}" if @whiny
       end
